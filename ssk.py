93aa8d0741dd15e109455529a7cd19ac9cabe78f
Emil Juzovitski <ejuzovitski@n129-p145.eduroam.kth.se>
2018-01-05 13:45:39 +0100

    first version

--- ssk.py
+++ ssk.py
@@ -10,15 +10,14 @@ def kPrimeN(sx,t,i):
     global lambdaDecay
     x = sx[-1]
     s = sx[:-1]
-
     sumJ = 0
+
     for j in range(0,len(t)):
+        print(j)
         if(t[j] == x):
+            print(j)
             sumJ += kPrimeN(s,t[:j-1],i - 1) * lambdaDecay ** (len(t) - j + 2)
-
     return lambdaDecay * kPrimeN(s,t,i) + sumJ
-
-    
 
 def kN(sx,t,i):
     if( min(len(sx),len(t)) < i):
@@ -27,13 +26,11 @@ def kN(sx,t,i):
     global lambdaDecay
     x = sx[-1]
     s = sx[:-1]
-
     sumJ = 0
 
     for j in range(0,len(t)):
         if(t[j] == x):
             sumJ += kPrimeN(sx,t[:j-1],i - 1) * lambdaDecay ** 2
-
     return kN(s,t,i) + sumJ
 
 stringS = 'car'
