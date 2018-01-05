56fddc35a12637058d5e75f5448693f0c66caf1b
hrstic <hrstic@kth.se>
2018-01-05 17:02:52 +0100

    Comparing running times
    Comparing the execution times of different implementations

--- sskPrime.py
+++ sskPrime.py
@@ -1,10 +1,10 @@
 import numpy as np
 import math as math
-import nltk as nltk
-from nltk.corpus import reuters
 import timeit
 
-def kPrimePrimeN(sx,t,i):
+
+
+def kPrimeN(sx,t,i):
     if(i == 0):
         return 1
     if( min(len(sx),len(t)) < i):
@@ -18,19 +18,7 @@ def kPrimePrimeN(sx,t,i):
     for j in range(0,len(t)):
         if(t[j] == x):
             sumJ += kPrimeN(s,t[:j],i - 1) * lambdaDecay ** (len(t) - j + 1)
-    return sumJ
-
-def kPrimeN(sx,t,i):
-    if(i == 0):
-        return 1
-    if( min(len(sx),len(t)) < i):
-        return 0
-
-    global lambdaDecay
-    x = sx[-1]
-    s = sx[:-1]
-
-    return lambdaDecay * kPrimeN(s,t,i) + kPrimePrimeN(sx,t,i)
+    return lambdaDecay * kPrimeN(s,t,i) + sumJ
 
 def kN(sx,t,i):
     if( min(len(sx),len(t)) < i):
@@ -46,28 +34,20 @@ def kN(sx,t,i):
             sumJ += kPrimeN(s,t[:j],i - 1) * (lambdaDecay ** 2)
     return kN(s,t,i) + sumJ
 
-#NOTE: If it doesn't work install nltk and add line nltk.download("reuters")
-#documentIDList = reuters.fileids()
-#print (reuters.raw(documentIDList[0]))
-
 stringS = 'carasdausdhaisdhisdhaisdhasidhaisdhaiuhdiahdiahsdusa'
 stringT = 'cataisdhaiuhdiauhsdiaushashdihdaiuhshdaudahidhammmmm'
 lambdaDecay = 0.5
 k = 2
 
-stringS = stringS.lower()
-stringT = stringT.lower()
 
-
-
-start = timeit.timeit()
+startPrime = timeit.timeit()
 notNormalized = kN(stringS,stringT,k)
-end = timeit.timeit()
-print("Running time for sskBis - ", end - start)
+endPrime = timeit.timeit()
+print("Running time - ", endPrime - startPrime)
 
+print(notNormalized)
 stringSKernel = kN(stringS,stringS,k)
 stringTKernel = kN(stringT,stringT,k)
 normalizedKernel = notNormalized / math.sqrt(stringSKernel * stringTKernel)
 
-print(notNormalized)
 print(normalizedKernel)
