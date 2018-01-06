3283c561b332109c664504563e179894c04d855c
ddiamant <DavidDiamant@n144-p199.eduroam.kth.se>
2018-01-06 10:29:01 +0100

    with testing

--- svm.py
+++ svm.py
@@ -21,8 +21,8 @@ test_docs_id = list(filter(lambda doc: doc.startswith("test"),
 train_docs_id = train_docs_id[:10]
 test_docs_id = test_docs_id[:10]
  
-train_docs = [reuters.raw(doc_id) for doc_id in train_docs_id]
-test_docs = [reuters.raw(doc_id) for doc_id in test_docs_id]
+train_docs = [reuters.raw(doc_id)[:10] for doc_id in train_docs_id]
+test_docs = [reuters.raw(doc_id)[:10] for doc_id in test_docs_id]
 
 train_labels = [reuters.categories(doc_id)
                                   for doc_id in train_docs_id]
@@ -33,7 +33,7 @@ gram = np.zeros((len(train_docs),len(train_docs)))
 
 for i in range(0,len(train_docs)):
 	for j in range(0, len(train_docs)):
-		gram[i][j] = ssk.kN(train_docs[i],train_docs[j],2)
+		gram[i][j] = ssk.kN(train_docs[i],train_docs[j],4)
 
 # TODO:
 "Construct the x and y, look at the labels and how they have done it in the report"
@@ -42,7 +42,21 @@ le = preprocessing.LabelEncoder()
 le.fit(Y)
 Y = le.transform(Y)
 
-clf = svm.SVC(kernel='precomputed')
-clf.fit(gram, Y)
+model = svm.SVC(kernel='precomputed')
+model.fit(gram, Y)
 
-#clf.predict()
+test_gram = np.zeros((len(test_docs),len(train_docs)))
+for i in range(0, len(test_docs)):
+	for j in range(0, len(train_docs)):
+		test_gram[i][j] = ssk.kN(test_docs[i],train_docs[i], 4)
+
+predicted = model.predict(test_gram)
+
+Y = np.array(test_labels)
+le = preprocessing.LabelEncoder()
+le.fit(Y)
+Y = le.transform(Y)
+
+print(np.mean(predicted == Y))
+
+#model.predict()
