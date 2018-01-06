2a19e61530efc69001b6dd20baf0851f1f5de590
ddiamant <DavidDiamant@n144-p199.eduroam.kth.se>
2018-01-06 14:34:40 +0100

    fixing

--- svm.py
+++ svm.py
@@ -6,6 +6,7 @@ import nltk as nltk
 from nltk.corpus import reuters
 from nltk.corpus import stopwords
 import numpy as np
+import math
 import re
 import ssk
 
@@ -53,7 +54,7 @@ def get_spam():
 
 	for i, filename in enumerate(os.listdir(path_test)):
 		with open(path_test+filename,'r') as email:
-			test_data[i] = email.read()
+			test_data[i] = email.read()[:200]
 		if("spmsgc" in filename):
 			test_labels[i] = 1
 		else:
@@ -61,7 +62,7 @@ def get_spam():
 
 	for i, filename in enumerate(os.listdir(path_train)):
 		with open(path_train+filename,'r') as email:
-			train_data[i] = email.read()
+			train_data[i] = email.read()[:200]
 		if("spmsgc" in filename):
 			train_labels[i] = 1
 		else:
@@ -76,14 +77,28 @@ def ssk_kernel(X, Y):
 	print(Y)
 
 sys.setrecursionlimit(10000)
+k = 10
 
 #test_docs, train_docs, train_labels, test_labels = get_reuters()
 test_docs, train_docs, train_labels, test_labels = get_spam()
+test_docs = test_docs[:2]
+train_docs = train_docs[:2]
+test_labels = test_labels[:2]
+train_labels = train_labels[:2]
+
+print(test_docs)
 
 gram = np.zeros((len(train_docs),len(train_docs)))
 for i in range(0,len(train_docs)):
+	for j in range(i, len(train_docs)):
+		gram[i][j] = ssk.kN(train_docs[i],train_docs[j], k)
+		print("new ssk done")
+		gram[j][i] = gram[i][j]
+
+#normalize
+for i in range(0,len(train_docs)):
 	for j in range(0, len(train_docs)):
-		gram[i][j] = ssk.kN(train_docs[i],train_docs[j],4)
+		gram[i][j] = gram[i][j]/math.sqrt(gram[i][i]/gram[j][j])
 
 # TODO:
 "Construct the x and y, look at the labels and how they have done it in the report"
@@ -98,7 +113,12 @@ model.fit(gram, Y)
 test_gram = np.zeros((len(test_docs),len(train_docs)))
 for i in range(0, len(test_docs)):
 	for j in range(0, len(train_docs)):
-		test_gram[i][j] = ssk.kN(test_docs[i],train_docs[j], 4)
+		test_gram[i][j] = ssk.kN(test_docs[i],train_docs[j], k)
+
+#normalize
+for i in range(0,len(test_docs)):
+	for j in range(0, len(test_docs)):
+		test_gram[i][j] = test_gram[i][j]/math.sqrt(test_gram[i][i]/test_gram[j][j])
 
 predicted = model.predict(test_gram)
 
