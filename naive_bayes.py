820f8353b89d9207d1536d62daeab23f805aea46
hrstic <hrstic@kth.se>
2018-01-14 14:24:48 +0100

    Modifications

--- naive_bayes.py
+++ naive_bayes.py
@@ -51,17 +51,28 @@ from sklearn.metrics import confusion_matrix
 #         docID = docID + 1
 #     return features_matrix
 
-def get_labels_dictionary():
+def get_labels_dictionary(train_count, test_count):
     all_words = []
-    sms_labels = []
+    train_labels = []
+    test_labels = []
     with open('smss.txt') as smsfile:
         for i,line in enumerate(smsfile):
+            if train_count != None:
+                if (i == train_count + test_count):
+                    break
             words = line.split()
             if words[0] == 'spam':
-                sms_labels += [1]
+                if(i < train_count):
+                    train_labels += [1]
+                    all_words += words
+                else:
+                    test_labels += [1]
             if words[0] == 'ham':
-                sms_labels += [0]
-            all_words += words
+                if (i < train_count):
+                    train_labels += [0]
+                    all_words += words
+                else:
+                    test_labels += [0]
 
     dictionary = Counter(all_words)
     del dictionary['spam']
@@ -73,42 +84,51 @@ def get_labels_dictionary():
             del dictionary[item]
         elif len(item) == 1:
             del dictionary[item]
-    dictionary = dictionary.most_common(2000)
 
-    return sms_labels, dictionary
+    dictionary = dictionary.most_common(len(dictionary))
 
-def extract_features(dictionary, sms_count):
-    features_matrix = np.zeros((sms_count,2000))
+    return train_labels,test_labels, dictionary
+
+def extract_features(dictionary, start_index, end_index):
+    features_matrix = np.zeros((end_index ,len(dictionary)))
     docID = 0
     with open('smss.txt') as datafile:
         for i,line in enumerate(datafile):
-            words = line.split()
-            del words[0]
-            for word in words:
-                wordID = 0
-                for i,d in enumerate(dictionary):
-                    if d[0] == word:
-                        wordID = i
-                        features_matrix[docID,wordID] = words.count(word)
-            docID = docID + 1
+            if (i == end_index + start_index):
+                break
+            if (i >= start_index):
+                words = line.split()
+                del words[0]
+                for word in words:
+                    wordID = 0
+                    for i,d in enumerate(dictionary):
+                        if d[0] == word:
+                            wordID = i
+                            features_matrix[docID,wordID] = words.count(word)
+                docID = docID + 1
     return features_matrix
 
-#Global variable for the length of substrings
-#key = 4
 def main():
     # Create a dictionary of words with its frequency
-    train_labels, dictionary = get_labels_dictionary()
-    sms_count = len(train_labels)
+    train_count = 1000
+    test_count = 500
+    train_labels, test_labels, dictionary = get_labels_dictionary(train_count, test_count)
     # Prepare feature vectors per training mail and its labels
-    train_matrix = extract_features(dictionary, sms_count)
+    train_matrix = extract_features(dictionary,0, train_count)
     # Training Naive bayes classifier
     model = MultinomialNB()
     model.fit(train_matrix,train_labels)
 
     # Test the unseen mails for Spam
-    test_matrix = extract_features(dictionary, sms_count)
+    test_matrix = extract_features(dictionary,train_count, test_count)
     result = model.predict(test_matrix)
-
-    print (confusion_matrix(train_labels,result))
+    # for i in range(sms_count):
+    #     if train_labels[i] != result[i]:
+    #          with open('smss.txt') as datafile:
+    #              for j,line in enumerate(datafile):
+    #                  if (i == j):
+    #                      print(line)
+    #                      break
+    print (confusion_matrix(test_labels,result))
 
 main()
